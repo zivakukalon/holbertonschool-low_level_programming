3e6b7388877d7b4d20fba32e88ccba31e90c27dd
zivakukalon <acucalon@misena.edu.co>
2020-09-17 07:05:48 +0000

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+/* HELLO, size */
+
+#include <stdio.h>
+
+/**
+ * main - file to print the size of the data / integrer
+ *
+ * Return: 0 
+ */
+
+int main(void)
+{
+  printf("Size of a char:%d byte(s)\n",size(char));
+  printf("Size of a int:%d byte(s)\n",size(int))
+  printf("Size of a long int:%d byte(s)\n",size(lon int));
+  printf("Size of a long long int:%d byte(s)\n",size(long long int));
+  printf("Size of a float:%d byte(s)\n",size(float));  
+  return (0);
+}
