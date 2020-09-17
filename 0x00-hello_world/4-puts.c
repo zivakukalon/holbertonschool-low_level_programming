855f76e0aeeb14d22cd0e447fd6bdef81b0630e0
zivakukalon <acucalon@misena.edu.co>
2020-09-17 05:01:11 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/* .. HELLO, PUTS ..*/
+
+#include <stdio.h>
+
+/**
+ * main - file to use the function puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts ("\Programming is like building a multilingual puzzle");
+    return (0);
+    }
