37cf89e2040c70cfc8f0d1041a721c281b5c6d71
zivakukalon <acucalon@misena.edu.co>
2020-09-17 06:25:02 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/* HELLO, PRINTF */
+
+#include <stdio.h>
+
+/**
+ * main - file to use the function puts
+ *
+ * Return: Always 0 (Success) 
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
